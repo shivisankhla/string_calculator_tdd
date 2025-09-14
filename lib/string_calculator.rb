208b1e696a5fb858019d832efbb8c2a43aefa0c2
Shivani <shivisankhla@gmail.com>
2025-09-14 20:36:22 +0530

    Step 11 refactored code

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,18 @@
 class StringCalculator
   def self.add(input)
-  	raise ArgumentError, "Input must be a string" unless input.is_a?(String)
+  	check_string(input)
     return 0 if input.empty?
     nums = input.split(/,|\n/).map(&:to_i)
+    check_negatives!(nums)
     nums.sum
+  end
+
+  def self.check_string(input)
+  	raise ArgumentError, "Input must be a string" unless input.is_a?(String)
+  end
+
+  def self.check_negatives!(nums)
+    negatives = nums.select(&:negative?)
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
   end
 end
