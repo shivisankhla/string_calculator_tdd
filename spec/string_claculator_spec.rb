6adf5a872c0ae93076732688831660c32a06fa0e
Shivani <shivisankhla@gmail.com>
2025-09-14 19:59:02 +0530

    Step 2 checks if its accepts argument

--- spec/string_claculator_spec.rb
+++ spec/string_claculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator do
     it "Checks for .add method" do
       expect(StringCalculator).to respond_to(:add)
     end
+
+    it "accepts a argument" do
+      expect { StringCalculator.add("1,2,3") }.not_to raise_error
+    end
   end
 end
