cf6e171cf7a947868a32e4d19c8e092f7d783931
Shivani <shivisankhla@gmail.com>
2025-09-14 20:18:02 +0530

    Step 8 spec to check newline as delimeters

--- spec/string_claculator_spec.rb
+++ spec/string_claculator_spec.rb
@@ -30,5 +30,9 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("1,2,3,4")).to eq(10)
     end
 
+    it "handles new lines as delimiters" do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
   end
 end
