62c380ba12fa91b5d37a359203b3f30072938926
Shivani <shivisankhla@gmail.com>
2025-09-14 20:19:50 +0530

    Step 9 added code for new line now checking for custome delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,6 @@ class StringCalculator
   def self.add(input)
     raise ArgumentError, "Input must be a string" unless input.is_a?(String)
     return 0 if input.empty?
-    input.split(",").map(&:to_i).sum
+    input.split(/,|\n/).map(&:to_i).sum
   end
 end
